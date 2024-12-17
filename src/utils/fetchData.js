94c3f3736728c8d74c814d9566e2256444ba2dfc
Shaik-Naheda24 <158743560+Shaik-Naheda24@users.noreply.github.com>
2024-12-17 17:08:07 +0530

    Full Stack Project
    full-stack application combining front-end (React) and back-end (Node.js) to create a small web app, such as a Exercise Tutoring Website.

---
+++ src/utils/fetchData.js
@@ -0,0 +1,29 @@
+export const exerciseOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+  },
+};
+
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'X-RapidAPI-Key': 'f0021db587msh781fb1cbef39856p11c183jsn45521d5d1c85',
+  },
+};
+
+export const fetchData = async (url, options) => {
+  try {
+    const response = await fetch(url, options);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Fetch error:', error);
+    return [];
+  }
+};
